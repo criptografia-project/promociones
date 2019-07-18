7edad01bc35b696ecdeec428385d60095f2e33d0
Camilo Nieto <caenietoba@unal.edu.co>
2019-07-18 01:26:28 +0000

    Fixed db port

--- src/modelos/tienda.js
+++ src/modelos/tienda.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 
 connection = mysql.createConnection({
     host            : process.env.DATABASE_HOST,
-    port            : process.env.MYSQL_PORT,
+    port            : 3306,
     user            : process.env.MYSQL_USER,
     password        : process.env.MYSQL_PASSWORD,
     database        : process.env.MYSQL_DATABASE
